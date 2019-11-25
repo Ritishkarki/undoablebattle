530eb16d65cdd4883502b128c8f20f8aaedcecd4
RitishKarki <karki.ritish@gmail.com>
2019-11-25 02:03:34 -0500

    reset all things

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <ctime>
-#include <vector>
-#include <iostream>
-using namespace std;
-int main(){
-    // for generating different set of random numbers every time
-    srand(time(0));
-    return 0;
-}
